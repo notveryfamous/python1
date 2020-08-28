bc0a6b40618c6f208d9d62381bb3b7c69520e6da
notveryfamous <Jerry1751@163.com>
2020-08-28 11:50:54 +0800

    2020.8.28 内建参数.py提交 函数的参数传递.py更改

---
+++ 内建函数.py
@@ -0,0 +1,10 @@
+# max()函数
+a = [2, 3, 5, 7, 9, 12, 25]
+maxx = max(a)
+print(maxx)
+
+# round函数
+a1 = 15.3
+b1 = 3
+c1 = a1 / b1
+print(round(c1, 2))
--- 函数的参数传递.py
+++ 函数的参数传递.py
@@ -8,11 +8,11 @@ def show(name, age, sex, hobby):  # 形参
 # 顺序传入
 show('张三', 18, '男', '打球')  # 实参
 
-
 # 关键词
 show(name='张三', sex='男', age=18, hobby='打球')  # 要么使用顺序传入，要么使用关键词
 
 
+# 默认参数
 def show(name, age, hobby, sex='男'):
     print('我叫：', name, '，年龄：', age, '，性别：', sex, '，爱好：', hobby)
 
@@ -27,4 +27,14 @@ def show(name, sex, hobby, age=22):
 show(name='张三', sex='男', age=18, hobby='打球')  # 以手动填写的参数为结果
 
 
-# 默认参数
+# 不定长参数
+def add(*args):
+    summ = 0
+    for i in args:
+        summ += i
+    return summ
+
+
+print(add(37, 555, 6))
+print(add())
+print(add(37, 555, 6, 5552, 5887447))
